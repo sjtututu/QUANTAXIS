d998501c305d52f6dd824b30a2d0851cc263ebbb
Tu Xinglong <415496929@qq.com>
2019-12-28 16:37:01 +0800

    alpha101和alpha191技术指标计算

---
+++ QUANTAXIS/QAAlpha/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@version: 1.0
+@author: Tu Xinglong
+@file: __init__.py
+@time: 2019/11/29 15:13
+"""
+
+
+def func():
+    pass
+
+
+class Main():
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
