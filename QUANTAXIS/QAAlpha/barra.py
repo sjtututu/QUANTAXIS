c16ef60d6cfa63a59b276477665819585e5fae5b
Tu Xinglong <415496929@qq.com>
2019-12-28 16:37:07 +0800

    alpha101和alpha191技术指标计算

---
+++ QUANTAXIS/QAAlpha/barra.py
@@ -0,0 +1,22 @@
+
+# sdjfls
+"""
+@version: 1.0
+@author: Tu Xinglong
+@file: barra.py
+@time: 2019/11/29 15:21
+"""
+
+
+def func():
+    pass
+
+
+class Main():
+    def __init__(self):
+        pass
+
+print("sdfsdf")
+
+if __name__ == '__main__':
+    print("中文")
