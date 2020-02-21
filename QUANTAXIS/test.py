a535ded2f53a53d36f6d2fb4ae01c7276c8cfa21
Tu Xinglong <415496929@qq.com>
2020-02-21 20:01:39 +0800

    alpha101和alpha191技术指标计算

---
+++ QUANTAXIS/test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@version: 1.0
+@author: Tu Xinglong
+@file: test.py
+@time: 2020/2/21 20:00
+"""
+
+
+def func():
+    pass
+
+
+class Main():
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
+
